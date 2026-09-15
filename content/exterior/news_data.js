39e15144599f2615370ce7355d7cbb87bba2b9b8
naiso1 <tgiid2gr5t@gmail.com>
2026-09-15 16:46:47 +0900

    Auto update daily news (2026-09-15)

---
+++ content/exterior/news_data.js
@@ -0,0 +1,39 @@
+window.LOADED_NEWS_DATA = [
+    // 2026-09-14 (google検索からExcel sheet2_llm_targets)
+    {
+        id: "jp1",
+        title: "レクサスLS改良でリアLマークがLEXUSバラ文字へ変更",
+        desc: "トヨタは2026年9月10日、フラッグシップセダン「LS」の一部改良モデルを発売した。リア中央のエンブレムが従来の「L」マークから「LEXUS」のバラ文字に変更され、SNSではカッコいいという好評と威厳が失われたという惜しむ声が交錯している。",
+        url: "https://news.yahoo.co.jp/articles/16a0805370a2422175e6e9cb042f8e45a9ede24d?source=rss",
+        source: "VAGUE",
+        date: "2026-09-14",
+        tags: ["エンブレム"],
+    
+        edition: "exterior", productScore: 85, exteriorScore: 85,
+        sourceExcerpt: "リア中央のブランドロゴが、「L」をかたどった従来仕様のエンブレムから、「LEXUS」の文字を一文字ずつ配置した“バラ文字エンブレム”に変更されています。",
+        interiorScore: 85,
+        interiorReason: "リアエンブレムのデザイン変更（Lマークからバラ文字へ）という具体的な外装コンポーネントの技術的・意匠的特徴が記事の中心であり、対象範囲に合致する。",
+        imageInterior: false,
+        country: "jp",
+        img: "https://newsatcl-pctr.c.yimg.jp/t/amd-img/20260915-10456791-vague-000-1-view.jpg",
+        note: ""
+    },
+    {
+        id: "us1",
+        title: "BMW X5にカーボン製グリルとアラミド製ディフューザーを追加",
+        desc: "BMWは2027年型X5向けに、カーボンファイバー製のグリルインサートやフロントスプリッター、アラミド素材のスポーティなディフューザーなど、M Performance Partsによる外装アクセサリーをラインナップした。",
+        url: "https://www.carscoops.com/2026/09/2027-bmw-x5-m-performace-parts/",
+        source: "CarScoops",
+        date: "2026-09-14",
+        tags: ["グリル", "材料"],
+    
+        edition: "exterior", productScore: 85, exteriorScore: 85,
+        sourceExcerpt: "Starting up front, customers can get carbon fiber grille inserts as well as a carbon fiber front splitter.",
+        interiorScore: 85,
+        interiorReason: "カーボンファイバー製のグリルインサートとスプリッター、アラミド製ディフューザーという外装部品の素材と設計が記載されている。",
+        imageInterior: false,
+        country: "us",
+        img: "https://www.carscoops.com/wp-content/uploads/2026/09/2027-BMW-X5-M-Performace-Pa-1024x578.jpg",
+        note: ""
+    },
+];
---
+++ content/exterior/source_list_data.js
@@ -0,0 +1,370 @@
+"use strict";
+
+// Generated from ニュース収集/rss_feed_list.csv. Do not edit manually.
+window.DAILYNEWS_CONFIGURED_SOURCES = [
+  {
+    "country": "日本",
+    "name": "レスポンス",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/rps/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "carview!",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/carv/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "MotorFan",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/motorfan/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "WEB CARTOP",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/wcartop/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "webCG",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/webcg/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "Auto Messe Web",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/amweb/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "VAGUE",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/vague/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "ベストカーWeb",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/bestcar/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "くるまのニュース",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/kurumans/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "日刊自動車新聞",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/netdenjd/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "THE EV TIMES",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/evtimes/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "AUTOCAR JAPAN",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/autocar/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "AUTO BILD JAPAN Web",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/autobild/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "ドライバーWeb",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/driverweb/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "MONOist",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/it_monoist/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "EE Times Japan",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/it_eetimes/all.xml"
+  },
+  {
+    "country": "日本",
+    "name": "Impress Watch",
+    "rssUrl": "https://news.yahoo.co.jp/rss/media/impress/all.xml"
+  },
+  {
+    "country": "米国",
+    "name": "The Drive",
+    "rssUrl": "https://www.thedrive.com/rss/all"
+  },
+  {
+    "country": "米国",
+    "name": "Motor1",
+    "rssUrl": "https://www.motor1.com/rss/news/all/"
+  },
+  {
+    "country": "米国",
+    "name": "CarScoops",
+    "rssUrl": "https://www.carscoops.com/feed/"
+  },
+  {
+    "country": "米国",
+    "name": "Jalopnik",
+    "rssUrl": "https://jalopnik.com/rss"
+  },
+  {
+    "country": "米国",
+    "name": "Car and Driver",
+    "rssUrl": "https://www.caranddriver.com/rss/all.xml/"
+  },
+  {
+    "country": "米国",
+    "name": "The Car Connection",
+    "rssUrl": "https://feeds.highgearmedia.com/?sites=TheCarConnection&type=all"
+  },
+  {
+    "country": "米国",
+    "name": "The Truth About Cars",
+    "rssUrl": "https://www.thetruthaboutcars.com/rss/feed/all"
+  },
+  {
+    "country": "米国",
+    "name": "The Torque Report",
+    "rssUrl": "https://www.thetorquereport.com/feed"
+  },
+  {
+    "country": "米国",
+    "name": "Top Speed",
+    "rssUrl": "https://www.topspeed.com/feed"
+  },
+  {
+    "country": "米国",
+    "name": "CarComplaints",
+    "rssUrl": "https://www.carcomplaints.com/news/feed.xml"
+  },
+  {
+    "country": "欧州",
+    "name": "AUTOCAR UK",
+    "rssUrl": "https://www.autocar.co.uk/rss"
+  },
+  {
+    "country": "欧州",
+    "name": "Auto Express",
+    "rssUrl": "https://www.autoexpress.co.uk/feed/all"
+  },
+  {
+    "country": "欧州",
+    "name": "Autocar Reviews",
+    "rssUrl": "https://www.autocar.co.uk/rss/reviews"
+  },
+  {
+    "country": "欧州",
+    "name": "Auto Express Car News",
+    "rssUrl": "https://www.autoexpress.co.uk/car-news/feed"
+  },
+  {
+    "country": "中国",
+    "name": "Autohome (CN)",
+    "rssUrl": "https://www.autohome.com.cn/rss/news.xml"
+  },
+  {
+    "country": "中国",
+    "name": "PCauto (CN)",
+    "rssUrl": "https://www.pcauto.com.cn/rss/news.xml"
+  },
+  {
+    "country": "中国",
+    "name": "CarNewsChina - All",
+    "rssUrl": "https://carnewschina.com/feed/"
+  },
+  {
+    "country": "中国",
+    "name": "CarNewsChina - EV",
+    "rssUrl": "https://carnewschina.com/category/electric-vehicles/feed/"
+  },
+  {
+    "country": "中国",
+    "name": "CarNewsChina - ICE",
+    "rssUrl": "https://carnewschina.com/category/internal-combustion-engine-vehicles/feed/"
+  },
+  {
+    "country": "中国",
+    "name": "CnEVPost - All",
+    "rssUrl": "https://cnevpost.com/feed/"
+  },
+  {
+    "country": "中国",
+    "name": "CnEVPost - Huawei",
+    "rssUrl": "https://cnevpost.com/huawei/feed"
+  },
+  {
+    "country": "中国",
+    "name": "CnEVPost - Xiaomi",
+    "rssUrl": "https://cnevpost.com/xiaomi/feed"
+  },
+  {
+    "country": "中国",
+    "name": "CnEVPost - Self-driving",
+    "rssUrl": "https://cnevpost.com/self-driving/feed"
+  },
+  {
+    "country": "中国",
+    "name": "CnEVPost - Industry",
+    "rssUrl": "https://cnevpost.com/industry/feed"
+  },
+  {
+    "country": "中国",
+    "name": "CnEVPost - Policy",
+    "rssUrl": "https://cnevpost.com/policy/feed"
+  },
+  {
+    "country": "中国",
+    "name": "Inside China Auto - All",
+    "rssUrl": "https://insidechinaauto.com/feed"
+  },
+  {
+    "country": "中国",
+    "name": "Gasgoo CN - Class 107",
+    "rssUrl": "https://auto.gasgoo.com/Rss/ClassRss.aspx?ClassId=107"
+  },
+  {
+    "country": "中国",
+    "name": "Gasgoo CN - Class 108",
+    "rssUrl": "https://auto.gasgoo.com/Rss/ClassRss.aspx?ClassId=108"
+  },
+  {
+    "country": "中国",
+    "name": "Gasgoo CN - Class 109",
+    "rssUrl": "https://auto.gasgoo.com/Rss/ClassRss.aspx?ClassId=109"
+  },
+  {
+    "country": "中国",
+    "name": "Gasgoo CN - Class 110",
+    "rssUrl": "https://auto.gasgoo.com/Rss/ClassRss.aspx?ClassId=110"
+  },
+  {
+    "country": "中国",
+    "name": "Gasgoo CN - Class 409",
+    "rssUrl": "https://auto.gasgoo.com/Rss/ClassRss.aspx?ClassId=409"
+  },
+  {
+    "country": "中国",
+    "name": "Gasgoo CN - Class 601",
+    "rssUrl": "https://auto.gasgoo.com/Rss/ClassRss.aspx?ClassId=601"
+  },
+  {
+    "country": "中国",
+    "name": "Gasgoo CN - Class 501",
+    "rssUrl": "https://auto.gasgoo.com/Rss/ClassRss.aspx?ClassId=501"
+  },
+  {
+    "country": "中国",
+    "name": "Gasgoo CN - Class 103",
+    "rssUrl": "https://auto.gasgoo.com/Rss/ClassRss.aspx?ClassId=103"
+  },
+  {
+    "country": "中国",
+    "name": "Gasgoo CN - Class 901",
+    "rssUrl": "https://auto.gasgoo.com/Rss/ClassRss.aspx?ClassId=901"
+  },
+  {
+    "country": "中国",
+    "name": "Gasgoo CN - Class 112",
+    "rssUrl": "https://auto.gasgoo.com/Rss/ClassRss.aspx?ClassId=112"
+  },
+  {
+    "country": "インド",
+    "name": "ETAuto - Auto Components",
+    "rssUrl": "https://auto.economictimes.indiatimes.com/rss/auto-components"
+  },
+  {
+    "country": "インド",
+    "name": "ETAuto - Auto Technology",
+    "rssUrl": "https://auto.economictimes.indiatimes.com/rss/auto-technology"
+  },
+  {
+    "country": "インド",
+    "name": "Autocar India - All",
+    "rssUrl": "https://www.autocarindia.com/rss/all"
+  },
+  {
+    "country": "インド",
+    "name": "RushLane",
+    "rssUrl": "https://www.rushlane.com/feed"
+  },
+  {
+    "country": "インド",
+    "name": "GaadiWaadi",
+    "rssUrl": "https://gaadiwaadi.com/feed"
+  },
+  {
+    "country": "インド",
+    "name": "IndiaCarNews",
+    "rssUrl": "https://www.indiacarnews.com/news/feed"
+  },
+  {
+    "country": "インド",
+    "name": "Car Blog India",
+    "rssUrl": "https://www.carblogindia.com/feed"
+  },
+  {
+    "country": "インド",
+    "name": "Auto Punditz",
+    "rssUrl": "https://www.autopunditz.com/blog-feed.xml"
+  },
+  {
+    "country": "米国",
+    "name": "InsideEVs",
+    "rssUrl": "https://insideevs.com/rss/news/all/"
+  },
+  {
+    "country": "米国",
+    "name": "Green Car Reports",
+    "rssUrl": "https://feeds.highgearmedia.com/?sites=GreenCarReports&type=all"
+  },
+  {
+    "country": "米国",
+    "name": "Electrek",
+    "rssUrl": "https://electrek.co/feed/"
+  },
+  {
+    "country": "欧州",
+    "name": "Electrive",
+    "rssUrl": "https://www.electrive.com/feed/"
+  },
+  {
+    "country": "欧州",
+    "name": "Just Auto",
+    "rssUrl": "https://www.just-auto.com/feed/"
+  },
+  {
+    "country": "欧州",
+    "name": "Automotive World",
+    "rssUrl": "https://www.automotiveworld.com/feed/"
+  },
+  {
+    "country": "欧州",
+    "name": "Electric Cars Report",
+    "rssUrl": "https://electriccarsreport.com/feed/"
+  },
+  {
+    "country": "インド",
+    "name": "IndianAutosBlog",
+    "rssUrl": "https://indianautosblog.com/feed"
+  },
+  {
+    "country": "インド",
+    "name": "MotorBeam",
+    "rssUrl": "https://www.motorbeam.com/feed/"
+  },
+  {
+    "country": "欧州",
+    "name": "Auto & Design",
+    "rssUrl": "https://autodesignmagazine.com/en/feed/"
+  },
+  {
+    "country": "米国",
+    "name": "BMWBLOG",
+    "rssUrl": "https://www.bmwblog.com/feed/"
+  },
+  {
+    "country": "日本",
+    "name": "ル・ボラン",
+    "rssUrl": "https://levolant.jp/feed/"
+  }
+];
